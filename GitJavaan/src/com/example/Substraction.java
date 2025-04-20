de1131dd6b9242ba342c79f5cb2bffd5c7a8b428
rajashri6 <rajashridhobale01@gmail.com>
2025-04-20 11:31:00 +0530

    Substration class added

---
+++ GitJavaan/src/com/example/Substraction.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 4;
+		int c = a - b;
+		System.out.println("Sub "+c);
+	}
+}
